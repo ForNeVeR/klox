921dfb2e84e8b00f8b2742442d2fe5d98fbd4344
Friedrich von Never <friedrich@fornever.me>
2024-10-26 23:11:27 +0200

    AstPrinter: implement Reverse Polish Notation

--- app/src/main/kotlin/AstPrinter.kt
+++ app/src/main/kotlin/AstPrinter.kt
@@ -18,11 +18,26 @@ class AstPrinter : Expr.Visitor<String> {
         "($name ${exprs.joinToString(" ") { it.accept(this) }})"
 }
 
+class ReversePrinter : Expr.Visitor<String> {
+    fun print(expr: Expr) = expr.accept(this)
+
+    override fun visitBinaryExpr(expr: Binary): String =
+        print(expr.left) + " " + print(expr.right) + " " + expr.operator.lexeme
+    override fun visitGroupingExpr(expr: Grouping): String =
+        print(expr)
+    override fun visitLiteralExpr(expr: Literal): String = when (expr.value) {
+        null -> "nil"
+        else -> expr.value.toString()
+    }
+    override fun visitUnaryExpr(expr: Unary): String =
+        print(expr.right) + " " + expr.operator.lexeme
+}
+
 fun main() {
     val expression = Binary(
-        Unary(Token(TokenType.MINUS, "-", null, 1), Literal(123)),
+        Binary(Literal(1.0), Token(TokenType.PLUS, "+", null, 1), Literal(2.0)),
         Token(TokenType.STAR, "*", null, 1),
-        Grouping(Literal(45.67))
+        Binary(Literal(4.0), Token(TokenType.MINUS, "-", null, 1), Literal(3.0))
     )
-    println(AstPrinter().print(expression))
+    println(ReversePrinter().print(expression))
 }
