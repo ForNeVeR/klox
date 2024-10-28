1663d77d85ece7f34440ec9329d28c78f7f2bc3d
Friedrich von Never <friedrich@fornever.me>
2024-10-28 23:37:53 +0100

    Chapter 6: first steps in the parser

--- app/src/main/kotlin/Expr.kt
+++ app/src/main/kotlin/Expr.kt
@@ -4,7 +4,7 @@
 
 package me.fornever.klox
 
-abstract class Expr {
+sealed class Expr {
     interface Visitor<R> {
         fun visitBinaryExpr(expr: Binary): R
         fun visitGroupingExpr(expr: Grouping): R
@@ -13,22 +13,22 @@ abstract class Expr {
     }
 
     abstract fun <R> accept(visitor: Visitor<R>): R
-}
 
-data class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr() {
-    override fun <R> accept(visitor: Visitor<R>): R =
-        visitor.visitBinaryExpr(this)
-}
+    data class Binary(val left: Expr, val operator: Token, val right: Expr) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R =
+            visitor.visitBinaryExpr(this)
+    }
 
-data class Grouping(val expression: Expr) : Expr() {
-    override fun <R> accept(visitor: Visitor<R>): R =
-        visitor.visitGroupingExpr(this)
-}
-data class Literal(val value: Any?) : Expr() {
-    override fun <R> accept(visitor: Visitor<R>): R =
-        visitor.visitLiteralExpr(this)
-}
-data class Unary(val operator: Token, val right: Expr) : Expr() {
-    override fun <R> accept(visitor: Visitor<R>): R =
-        visitor.visitUnaryExpr(this)
+    data class Grouping(val expression: Expr) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R =
+            visitor.visitGroupingExpr(this)
+    }
+    data class Literal(val value: Any?) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R =
+            visitor.visitLiteralExpr(this)
+    }
+    data class Unary(val operator: Token, val right: Expr) : Expr() {
+        override fun <R> accept(visitor: Visitor<R>): R =
+            visitor.visitUnaryExpr(this)
+    }
 }
