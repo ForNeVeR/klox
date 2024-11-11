c829f4ad31610663fef410d7252f6891b030708f
Friedrich von Never <friedrich@fornever.me>
2024-11-11 23:35:51 +0100

    Interpreter: runtime error handling

---
+++ app/src/main/kotlin/RuntimeError.kt
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.me>
+//
+// SPDX-License-Identifier: MIT
+
+package me.fornever.klox
+
+class RuntimeError(internal val token: Token, message: String) : RuntimeException(message)
