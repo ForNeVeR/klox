53955eb6a541a5ddf95b34ccffba975a99412c35
Friedrich von Never <friedrich@fornever.me>
2024-12-23 00:03:20 +0100

    Licenses: annotate the new files

--- app/src/main/kotlin/Return.kt
+++ app/src/main/kotlin/Return.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Friedrich von Never <friedrich@fornever.me>
+//
+// SPDX-License-Identifier: MIT
+
 package me.fornever.klox
 
 class Return(val value: Any?) : RuntimeException(null, null, false, false)
